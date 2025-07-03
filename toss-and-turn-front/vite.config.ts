199715daaf2119b79ae331315bed0876c72c20f8
beomwon <beomwon@naver.com>
2025-07-03 10:45:05 +0900

    [수정] homepage 경로 재수정 - 캐시문제로 제대로 페이지 로딩이 안되어 복구하여 해결

--- toss-and-turn-front/vite.config.ts
+++ toss-and-turn-front/vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // GitHub Pages root 경로
+  base: "/study/", // GitHub Pages 서브디렉토리 경로
 });
