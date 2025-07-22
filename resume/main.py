6c35f0793e45f691010b01a37b87bcf953e5fb1a
beomwon <beomwon@naver.com>
2025-07-22 17:05:27 +0900

    [resume] [v1.0] [추가] health 체크 추가

--- resume/main.py
+++ resume/main.py
@@ -12,12 +12,16 @@ app = FastAPI()
 app.mount("/front/static", StaticFiles(directory="front/static"), name="static")
 templates = Jinja2Templates(directory="front/templates")
 
+# 기능별 라우터 등록
+app.include_router(practice.router, prefix="/practice")
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# 기능별 라우터 등록
-app.include_router(practice.router, prefix="/practice")
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
 
 # 개발용 서버 실행 코드
 # if __name__ == "__main__":
