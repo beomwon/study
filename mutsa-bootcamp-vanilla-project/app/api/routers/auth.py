d1ec07314bab7cf9b1f60089657c2236e92a0f80
beomwon <beomwon@naver.com>
2025-07-25 16:01:21 +0900

    [mutsa] [v1.1] [수정] await 수정

--- mutsa-bootcamp-vanilla-project/app/api/routers/auth.py
+++ mutsa-bootcamp-vanilla-project/app/api/routers/auth.py
@@ -19,12 +19,12 @@ class AuthCodeRequest(BaseModel):
 
 @router.post("/sign-up/auth")
 async def send_auth_code(data: AuthCodeRequest):
-    return await auth_service.send_auth_code(data.email)
+    return auth_service.send_auth_code(data.email)
 
 @router.post("/sign-up")
 async def sign_up(data: SignUpRequest):
-    return await auth_service.sign_up(data.email, data.password)
+    return auth_service.sign_up(data.email, data.password)
 
 @router.post("/login")
 async def login(data: LoginRequest):
-    return await auth_service.login(data)
+    return auth_service.login(data)
