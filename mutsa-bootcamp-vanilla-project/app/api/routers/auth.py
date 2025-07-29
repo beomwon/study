64cbd577348328646d8b26243b0497ad316494d5
beomwon <beomwon@naver.com>
2025-07-29 13:34:02 +0900

    [mutsa] [v1.1] [추가] 닉네임 설정 추가

--- mutsa-bootcamp-vanilla-project/app/api/routers/auth.py
+++ mutsa-bootcamp-vanilla-project/app/api/routers/auth.py
@@ -17,12 +17,16 @@ class LoginRequest(BaseModel):
 class AuthCodeRequest(BaseModel):
     email: str
 
+class UpdateNicknameRequest(BaseModel):
+    email: str
+    nickname: str
+
 @router.post("/sign-up/auth")
 async def send_auth_code(data: AuthCodeRequest):
     return auth_service.send_auth_code(data.email)
 
 @router.post("/sign-up/update_user_nickname")
-async def update_user_nickname(data):
+async def update_user_nickname(data: UpdateNicknameRequest):
     return auth_service.update_user_nickname(data.email, data.nickname)
 
 @router.post("/sign-up")
