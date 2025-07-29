a3da08fb6477d6ef9797516d76db36ffd6864482
beomwon <beomwon@naver.com>
2025-07-29 14:35:13 +0900

    [mutsa] [v1.1] [추가] 닉네임 설정 추가

--- mutsa-bootcamp-vanilla-project/app/services/supabase.py
+++ mutsa-bootcamp-vanilla-project/app/services/supabase.py
@@ -1,7 +1,11 @@
+import re
 from supabase import create_client
 from config.settings import settings
 from datetime import date
-import os
+import os, logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # supabase = create_client(settings.SUPABASE_URL, os.environ.get("SUPABASE_SERVICE_ROLE_KEY"))
 supabase = create_client(settings.SUPABASE_URL, settings.SUPABASE_SERVICE_ROLE_KEY)
@@ -30,7 +34,12 @@ def update_user_nickname(email: str, nickname: str):
     if not user:
         return None
     
-    resp = supabase.table("users").update({"nickname": nickname}).eq("email", email).execute()
+    logger.info(f"Updating nickname for user: {email} to {nickname}")
+    try:
+        resp = supabase.table("users").update({"nickname": nickname}).eq("email", email).execute()
+    except Exception as e:
+        logger.error(f"Error updating nickname: {e}")
+        return None
     if resp.data and len(resp.data) > 0:
         return resp.data[0]
     return None
